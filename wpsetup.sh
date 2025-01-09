a6c79a8e965f0743611a4a4d73e4c679078eb19d
Mats De Beer <mde-beer@f1r3s20.codam.nl>
2025-01-09 19:56:22 +0100

    the entirety of the config is now inside my shell script. cuz fuck you

--- wpsetup.sh
+++ wpsetup.sh
@@ -27,10 +27,26 @@ sudo systemctl restart php-fpm
 sudo systemctl restart lighttpd
 #sed -i'' -Ee 's/@reboot.*//' /var/spool/cron/root
 sudo sed -i'' -Ee "s/define\(.*\'DB_NAME.*//" -e "s/define\(.*\'DB_USER.*//" -e "s/define\(.*\'DB_PASSWORD.*//" -e "s/define\(.*\'DB_HOST.*//" -e "s/require_once.*//" /var/www/lighttpd/wp-config.php
-echo "define('DB_NAME', 'wordpress');
+echo "<?php
+define( 'DB_CHARSET', 'utf8' );
+define( 'DB_COLLATE', '' );
+define( 'AUTH_KEY',         'put your unique phrase here' );
+define( 'SECURE_AUTH_KEY',  'put your unique phrase here' );
+define( 'LOGGED_IN_KEY',    'put your unique phrase here' );
+define( 'NONCE_KEY',        'put your unique phrase here' );
+define( 'AUTH_SALT',        'put your unique phrase here' );
+define( 'SECURE_AUTH_SALT', 'put your unique phrase here' );
+define( 'LOGGED_IN_SALT',   'put your unique phrase here' );
+define( 'NONCE_SALT',       'put your unique phrase here' );
+\$table_prefix = 'wp_';
+define( 'WP_DEBUG', false );
+if ( ! defined( 'ABSPATH' ) ) {
+        define( 'ABSPATH', __DIR__ . '/' );
+}
+define('DB_NAME', 'wordpress');
 define('DB_USER', 'wpuser');
 define('DB_PASSWORD', 'Born2beauto');
 define('DB_HOST', 'localhost');
-require_once ABSPATH . 'wp-settings.php';" >> /var/www/lighttpd/wp-config.php
+require_once ABSPATH . 'wp-settings.php';" > /var/www/lighttpd/wp-config.php
 rm -f /bin/wpsetup.sh
 echo "cleanup done!"
