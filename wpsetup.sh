789663212b2626adb306df15e5b95e54f99334f7
Mats De Beer <mde-beer@f1r3s20.codam.nl>
2025-01-09 18:12:32 +0100

    messages!

--- wpsetup.sh
+++ wpsetup.sh
@@ -1,10 +1,10 @@
 #/bin/sh
 
+echo "restarting services"
 sudo systemctl restart mariadb
 sudo systemctl restart php-fpm
 sudo systemctl restart lighttpd
-sleep 10
-
+echo "services restarted"
 sudo mysql_secure_installation <<EOF
 
 n
@@ -21,9 +21,11 @@ CREATE USER 'wpuser'@'localhost' IDENTIFIED BY 'Born2beauto';
 GRANT ALL PRIVILEGES ON wordpress.* TO 'wpuser'@'localhost';
 FLUSH PRIVILEGES;
 EOF
-
+echo "databases initialised"
 sudo systemctl restart mariadb
 sudo systemctl restart php-fpm
 sudo systemctl restart lighttpd
 sed -i'' -Ee 's/@reboot.*//' /var/spool/cron/root
 rm -f /bin/wpsetup.sh
+
+echo "cleanup done!"
