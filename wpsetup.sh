3b2231d967505dc0914d393f6097bf20b3f5d71b
Mats De Beer <mde-beer@f1r3s20.codam.nl>
2025-01-09 19:34:34 +0100

    fixed wrong path in wpsetup

--- wpsetup.sh
+++ wpsetup.sh
@@ -26,11 +26,11 @@ sudo systemctl restart mariadb
 sudo systemctl restart php-fpm
 sudo systemctl restart lighttpd
 #sed -i'' -Ee 's/@reboot.*//' /var/spool/cron/root
-sudo sed -i'' -Ee "s/define\(.*\'DB_NAME.*//" -e "s/define\(.*\'DB_USER.*//" -e "s/define\(.*\'DB_PASSWORD.*//" -e "s/define\(.*\'DB_HOST.*//" -e "s/require_once.*//" /var/www/html/wp-config.php
+sudo sed -i'' -Ee "s/define\(.*\'DB_NAME.*//" -e "s/define\(.*\'DB_USER.*//" -e "s/define\(.*\'DB_PASSWORD.*//" -e "s/define\(.*\'DB_HOST.*//" -e "s/require_once.*//" /var/www/lighttpd/wp-config.php
 echo "define('DB_NAME', 'wordpress');
 define('DB_USER', 'wpuser');
 define('DB_PASSWORD', 'Born2beauto');
 define('DB_HOST', 'localhost');
-require_once ABSPATH . 'wp-settings.php';" >> /var/www/html/wp-config.php
+require_once ABSPATH . 'wp-settings.php';" >> /var/www/lighttpd/wp-config.php
 rm -f /bin/wpsetup.sh
 echo "cleanup done!"
