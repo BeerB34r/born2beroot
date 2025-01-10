05cfdf3040b6eb2adb61af350651eaf2d6bba7a8
Mats De Beer <mde-beer@f1r3s20.codam.nl>
2025-01-10 13:16:59 +0100

    changed to video dir to /videos

--- wpsetup.sh
+++ wpsetup.sh
@@ -58,12 +58,11 @@ sudo dnf install -y jellyfin
 sudo systemctl enable --now jellyfin
 sudo firewall-cmd --permanent --add-service=jellyfin
 sudo firewall-cmd --reload
-mkdir /home/mde-beer/videos
-chmod -R 766 /home/mde-beer
-chown jellyfin:jellyfin /home/mde-beer/videos
-wget -O /home/mde-beer/videos/johnnybravo1997.zip 'https://archive.org/compress/johnnybravo1997/formats=MPEG4&file=/johnnybravo1997.zip'
-unzip /home/mde-beer/videos/johnnybravo1997.zip -d /home/mde-beer/videos
-chmod -R 766 /home/mde-beer/videos
+mkdir /videos
+chown jellyfin:jellyfin /videos
+wget -O /videos/johnnybravo1997.zip 'https://archive.org/compress/johnnybravo1997/formats=MPEG4&file=/johnnybravo1997.zip'
+unzip /home/mde-beer/videos/johnnybravo1997.zip -d /videos
+chmod -R 666 /videos
 systemctl restart jellyfin
 rm -f /bin/wpsetup.sh
 echo "cleanup done!"
